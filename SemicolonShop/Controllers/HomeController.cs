3821f3d0a7d1c8f745b22dd0534199a5ece09238
noojob001 <noojob001@outlook.com>
2020-11-17 23:47:53 +0700

    logincom

--- SemicolonShop/Controllers/HomeController.cs
+++ SemicolonShop/Controllers/HomeController.cs
@@ -28,6 +28,16 @@ namespace SemicolonShop.Controllers
             return View();
         }
 
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
