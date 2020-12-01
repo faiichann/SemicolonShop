aacf97d9d0f3385cf4d56ced49c4436b36be813d
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2020-12-02 00:34:03 +0700

    add page

---
+++ SemicolonShop/Controllers/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemicolonShop.Controllers
+{
+    public class User : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Profile()
+        {
+            return View();
+        }
+        public IActionResult Editprofile()
+        {
+            return View();
+        }
+        public IActionResult History()
+        {
+            return View();
+        }
+    }
+}
