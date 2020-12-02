42fd1a181f045d30bdbcbc69cb314b063cd4bed3
Nichkamon Promputta <fainichkamon@gmail.com>
2020-12-03 04:54:58 +0700

    CRUD

--- SemicolonShop/Controllers/HomeController.cs
+++ SemicolonShop/Controllers/HomeController.cs
@@ -5,219 +5,237 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using SemicolonShop.Models;
+using ShopOnline.Models;
+using ShopOnline.Models.db;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Mvc.Rendering;
-using System.Dynamic;
+using System.IO;
 
-namespace SemicolonShop.Controllers
+namespace ShopOnline.Controllers
 {
     public class HomeController : Controller
     {
-   //     [NonAction]
-   //     public List<User> GetLoginUserList()
-   //     {
-   //         return new List<User>{
-   //   new User{
-   //      UserID =1,
-   //      UFirstName= "aaa",
-   //      ULastName="bbb",
-   //      UserName="admin",
-   //      Password= "1234"
-   //   },
-   //         new User{
-   //      UserID =2,
-   //      UFirstName= "faii",
-   //      ULastName="chann",
-   //      UserName="faiichann",
-   //      Password= "1111"
-   //   },
-   //};
-   //     }
+        private readonly ILogger<HomeController> _logger;
 
-   //     public List<Product> GetProduct()
-   //     {
-   //         List<Product> pd = new List<Product>();
-   //         pd.Add(new Product { ProductID = 1, ProductType = "Vet", ProductName = "carrot", ProductPrice = "20" , ProductUnit = "15" , DateUpdate = "12/02/12"});
-   //         pd.Add(new Product { ProductID = 2, ProductType = "Me", ProductName = "pig", ProductPrice = "120", ProductUnit = "30", DateUpdate = "12/02/12" });
-   //         pd.Add(new Product { ProductID = 3, ProductType = "Vet", ProductName = "onion", ProductPrice = "53", ProductUnit = "30", DateUpdate = "12/02/12" });
-   //         return pd;
-   //     }
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
 
-        public IActionResult Index()
+        SemicolonContext db = new SemicolonContext();
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            //    //Checking the state of model passed as parameter.
+            //    if (ModelState.IsValid)
+            //    {
+
+            //        //Validating the user, whether the user is valid or not.
+            //        var isValidUser = IsValidUser(user);
+
+            //        //If user is valid & present in database, we are redirecting it to Welcome page.
+            //        if (isValidUser != null)
+            //        {
+            //            FormsAuthentication.SetAuthCookie(user.UserPass, false);
+            //            return RedirectToAction("Index");
+            //        }
+            //        else
+            //        {
+            //            //If the username and password combination is not present in DB then error message is shown.
+            //            ModelState.AddModelError("Failure", "Wrong Username and password combination !");
+            //            return View();
+            //        }
+            //    }
+            //    else
+            //    {
+            //        //If model state is not valid, the model with error message is returned to the View.
+            //        return View(user);
+            //    }
+            //}
+            string btnCick = HttpContext.Request.Form["Login"].ToString();
+            if (btnCick == "Login")
+            {
+                string Username = HttpContext.Request.Form["username"].ToString();
+                string Password = HttpContext.Request.Form["password"].ToString();
+                //var login = (from e in GetLoginUserList()
+                //             where e.UserName == Username &&
+                //             e.Password == Password
+                //             select e).FirstOrDefault();
+                // if (login != null)
+                // {
+
+                if (Username == "admin" && Password == "1234")
+                {
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+
+                    return RedirectToAction("Register");
+                }
+                //}
+                // else
+                // return View("Index");
+            }
+            return View();
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Users.ToListAsync());
+        }
+
+        public IActionResult Register()
         {
             return View();
         }
-        //public IActionResult Admin()
-        //{
-        //    ViewBag.Username = "Admin";;
-        //    ViewBag.Product = GetProduct();
-        //    return View();
-        //}
-        //public IActionResult Home()
-        //{
-        //    ViewBag.Username = "User";
-        //    return View();
-        //}
-        //public IActionResult Register()
-        //{
-        //    ViewBag.Username = "User";
-        //    return View();
-        //}
+        public async Task<IActionResult> Profile(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //public IActionResult Add()
-        //{
-        //    ViewBag.Product = GetProduct();
-        //    return View();
-        //}
-        //public IActionResult Edit()
-        //{
-        //    ViewBag.Product = GetProduct();
-        //    return View();
-        //}
-        //public IActionResult Delete()
-        //{
-        //    ViewBag.Product = GetProduct();
-        //    return View();
-        //}
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Add([Bind("ProductID,ProductType,ProductName,ProductPrice,ProductUnit")] Product product)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        _context.Add(product);
-        //        await _context.SaveChangesAsync();
-        //        return RedirectToAction(nameof(Admin));
-        //    }
-        //    return View(product);
-        //}
+            var user = await db.Users
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
 
-        //// GET: CRUD/Edit/5
-        //public async Task<IActionResult> Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+            return View(user);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Obsolete]
+        public async Task<IActionResult> Register(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                //upload image
+                //if (user.UserImage != null && user.UserImage.Length > 0)
+                //{
+                //    var imagePath = @"\images\";
+                //    var uploadPath = _env.WebRootPath + imagePath;
 
-        //    var product = await _context.Product.FindAsync(id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
-        //    return View(product);
-        //}
+                //    if (!Directory.Exists(uploadPath))
+                //    {
+                //        Directory.CreateDirectory(uploadPath);
+                //    }
 
-        //// POST: CRUD/Edit/5
-        //// To protect from overposting attacks, enable the specific properties you want to bind to, for 
-        //// more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> Edit(int id, [Bind("ProductID,ProductType,ProductName,ProductPrice,ProductUnit")] Product product)
-        //{
-        //    if (id != product.ProductID)
-        //    {
-        //        return NotFound();
-        //    }
+                //    var uniqFileName = Guid.NewGuid().ToString();
+                //    var filename = Path.GetFileName(uniqFileName + "." + user.UserImage.Split(".")[1].ToLower());
+                //    string fullPath = uploadPath + filename;
 
-        //    if (ModelState.IsValid)
-        //    {
-        //        try
-        //        {
-        //            _context.Update(product);
-        //            await _context.SaveChangesAsync();
-        //        }
-        //        catch (DbUpdateConcurrencyException)
-        //        {
-        //            if (!ProductExists(product.ProductID))
-        //            {
-        //                return NotFound();
-        //            }
-        //            else
-        //            {
-        //                throw;
-        //            }
-        //        }
-        //        return RedirectToAction(nameof(Index));
-        //    }
-        //    return View(product);
-        //}
+                //    imagePath = imagePath + @"\";
+                //    var filePath = @".." + Path.Combine(imagePath, filename);
 
-        //// GET: CRUD/Delete/5
-        //public async Task<IActionResult> Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return NotFound();
-        //    }
+                //    using (var fileStream = new FileStream(fullPath, FileMode.Create))
+                //    {
+                //        await user.CopyToAsync(fileStream);
+                //    }
 
-        //    var product = await _context.Product
-        //        .FirstOrDefaultAsync(m => m.ProductID == id);
-        //    if (product == null)
-        //    {
-        //        return NotFound();
-        //    }
+                //    ViewData["FileLocation"] = filePath;
+                //}
+                //byte[] bytes;
+                //using (BinaryReader br = new BinaryReader(image.InputStream))
+                //{
+                //    bytes = br.ReadBytes(image.ContentLength);
+                //}
+                //db.Add(user{
+                //    user.UserImage = Path.GetFileName(image.FileName),
+                //    user.Data = bytes
+                //});
+                db.Add(user);
+                await db.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
 
-        //    return View(product);
-        //}
+            }
+            return View(user);
+        }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
 
-        //// POST: CRUD/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public async Task<IActionResult> DeleteConfirmed(int id)
-        //{
-        //    var product = await _context.Product.FindAsync(id);
-        //    _context.Product.Remove(product);
-        //    await _context.SaveChangesAsync();
-        //    return RedirectToAction(nameof(Index));
-        //}
+            var user = await db.Users.FindAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Login()
-        //{
-        //    string btnCick = HttpContext.Request.Form["Login"].ToString();
-        //    if (btnCick == "Login")
-        //    {
-        //        string Username = HttpContext.Request.Form["username"].ToString();
-        //        string Password = HttpContext.Request.Form["password"].ToString();
-        //        //var login = (from e in GetLoginUserList()
-        //        //             where e.UserName == Username &&
-        //        //             e.Password == Password
-        //        //             select e).FirstOrDefault();
-        //       // if (login != null)
-        //       // {
-        //            if (Username == "admin" && Password == "1234")
-        //            {
-                        
-        //                return RedirectToAction("Admin");
-        //            }
-        //            else
-        //            {
-                        
-        //                return RedirectToAction("Home");
-        //            }
-        //        //}
-        //       // else
-        //           // return View("Index");
-        //    }
-        //    return View();
-        //}
+        // POST: CRUD/Edit/5
 
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public IActionResult Register(User user)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        //db.Add(user);
-        //        //await db.SaveChangesAsync();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(user);
-        //}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserName,UserPass,UserImage,UserAddress,UserEmail,UserPhone,UserUpdate")] User user)
+        {
+            if (id != user.UserId)
+            {
+                return NotFound();
+            }
 
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    db.Update(user);
+                    await db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserExists(user.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
 
+        // GET: CRUD/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var user = await db.Users
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return View(user);
+        }
+
+        // POST: CRUD/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var user = await db.Users.FindAsync(id);
+            db.Users.Remove(user);
+            await db.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        private bool UserExists(int id)
+        {
+            return db.Users.Any(e => e.UserId == id);
+        }
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
