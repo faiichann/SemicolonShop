013491f353124dd5b92ad46a074e6b96faeefbb6
noojob001 <noojob001@outlook.com>
2020-12-02 22:49:03 +0700

    t

---
+++ SemicolonShop/Controllers/profileuser.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemicolonShop.Controllers
+{
+    public class profileuser : Controller
+    {
+        public IActionResult Personal()
+        {
+            return View();
+        }
+        public IActionResult Address()
+        {
+            return View();
+        }
+        public IActionResult Orderlist()
+        {
+            return View();
+        }
+        public IActionResult ChangePersonal()
+        {
+            return View();
+        }
+        public IActionResult ChangeAddress()
+        {
+            return View();
+        }
+    }
+}
--- SemicolonShop/Startup.cs
+++ SemicolonShop/Startup.cs
@@ -50,7 +50,7 @@ namespace SemicolonShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=profileuser}/{action=Personal}/{id?}");
             });
         }
     }
