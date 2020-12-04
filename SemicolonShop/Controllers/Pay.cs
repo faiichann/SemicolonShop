7f2c3c27a203763ebe1bc9a96a768cae6aae8421
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2020-12-05 04:16:17 +0700

    detail เสร็จแล้ว
    เหลือพวกตกแต่งให้สวยๆอ่ะ

---
+++ SemicolonShop/Controllers/Pay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemicolonShop.Controllers
+{
+    public class Pay : Controller
+    {
+        public IActionResult Payment()
+        {
+            return View();
+        }
+    }
+}
