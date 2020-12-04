5c6840594d5cb1ca93bfc4e4bb0c5482e1b496f2
Nichkamon Promputta <fainichkamon@gmail.com>
2020-12-05 02:09:35 +0700

    add js

---
+++ SemicolonShop/Models/Chart.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SemicolonShop.Models
+{
+        public partial class Chart
+        {
+            public string Month { get; set; }
+            public int Qty { get; set; }
+        }
+}
--- SemicolonShop/wwwroot/js/site.js
+++ SemicolonShop/wwwroot/js/site.js
@@ -1,4 +1,39 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+ $(function () {
+     $.ajax({
+         type: 'POST',
+         dataType: 'json',
+         contentType: 'application/json',
+         url: '@Url.Action("Piechart")',
+         data: '{}',
+         success: function (chartsdata) {
 
-// Write your JavaScript code.
+             // Callback that creates and populates a data table,    
+             // instantiates the pie chart, passes in the data and    
+             // draws it.    
+
+             var data = new google.visualization.DataTable();
+
+             data.addColumn('string', 'PlanName');
+             data.addColumn('number', 'PaymentAmount');
+
+             for (var i = 0; i < chartsdata.length; i++) {
+                 data.addRow([chartsdata[i].PlanName, chartsdata[i].PaymentAmount]);
+             }
+
+             // Instantiate and draw our chart, passing in some options    
+             var chart = new google.visualization.PieChart(document.getElementById('chartdiv'));
+
+             chart.draw(data,
+                 {
+                     title: "Show Google Chart in Asp.net",
+                     position: "top",
+                     fontsize: "14px",
+                     chartArea: { width: '50%' },
+                 });
+         },
+         error: function () {
+             alert("Error loading data! Please try again.");
+         }
+     });
+ })  
