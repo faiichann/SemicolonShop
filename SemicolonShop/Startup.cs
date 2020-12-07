c6d9223e2e2a72c801105abad9c7afeee946d2ac
ssomoo2 <72564324+ssomoo2@users.noreply.github.com>
2020-12-07 23:10:44 +0700

    footer เสร็จแล้ว

--- SemicolonShop/Startup.cs
+++ SemicolonShop/Startup.cs
@@ -50,7 +50,7 @@ namespace SemicolonShop
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Admin}/{action=Index}/{id?}");
             });
         }
     }
